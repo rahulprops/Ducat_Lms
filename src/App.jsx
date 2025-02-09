1daf79cc12da9f0990924dfd740280612c85ca8a
rahulprops <rahulhack912@gmail.com>
2025-02-09 15:12:02 +0530

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HeroSection from './pages/HeroSection.jsx';
+import FacutlyDashboard from './pages/FacutlyDashboard';
+import ToggleRegister from './pages/ToggleRegister';
+import AdminRegiseter from './components/admin/AdminRegiseter';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Header/>
-      <Footer/>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/dashboard" element={<FacutlyDashboard />} />
+        <Route path="/register" element={<ToggleRegister />} />
+        <Route path="/adminregister" element={<AdminRegiseter />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
