fd6a0588fef12e685955034754eb4d5dac47a406
rahulprops <rahulhack912@gmail.com>
2025-02-09 14:18:00 +0530

    toggleRegister login pages create

---
+++ src/components/FacultyRegister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FacultyRegister = () => {
+  return (
+    <div>FacultyRegister</div>
+  )
+}
+
+export default FacultyRegister
---
+++ src/pages/ToggleRegister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToggleRegister = () => {
+  return (
+    <div>ToggleRegister</div>
+  )
+}
+
+export default ToggleRegister
